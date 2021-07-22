c248dee438ad82b9afda2e761d75852bcfff69d3
ben91lin <ben91lin@gmail.com>
2021-07-22 23:25:19 +0800

    andd font family & set colors.

--- src/front-end/tailwind.config.js
+++ src/front-end/tailwind.config.js
@@ -8,11 +8,35 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Noto Sans TC'],
+      'serif': ['Noto Serif TC'],
+      },
     minHeight: {
       '8': '2rem',
       '16': '4rem',
       '32': '8rem'
     },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#F9FAF4',
+      'secondary': '#4A6163',
+      'pastel-orange': '#FFC94B'
+    }),
+    textColor: theme => ({
+      ...theme('colors'),
+      'primary': '#F9FAF4',
+      'secondary': '#4A6163',
+      'sandy-brown': '#F9A66C',
+      'pastel-orange': '#FFC94B',
+      'light-coral': '#F17A7E'
+    }),
+    gradientColorStops: theme => ({
+      ...theme('colors'),
+      'sandy-brown': '#F9A66C',
+      'pastel-orange': '#FFC94B',
+      'light-coral': '#F17A7E',
+    }),
     extend: {},
   },
   variants: {

ecbf24f81b04a261696154051eea7e4a91c40a81
ben91lin <ben91lin@gmail.com>
2021-07-24 00:31:29 +0800

    setFormFilterClass by isOpen & isFixed.

--- src/front-end/src/app/header/navigation/navigation.component.ts
+++ src/front-end/src/app/header/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter, ElementRef } from '@angular/core';
 
 import { FormService } from '../../services/form.service';
 import { Article } from 'src/app/board/model/article';
@@ -12,6 +12,7 @@ export class NavigationComponent implements OnInit {
 
   @Output() private onSetFormService: EventEmitter<FormService>;
   @Output() private onSetArticles: EventEmitter<Article[]>;
+  @Input() public isFixed!: boolean;
   public isOpen: Boolean;
 
   constructor(
@@ -27,6 +28,17 @@ export class NavigationComponent implements OnInit {
 
   setIsOpen(event: Boolean):void {
     this.isOpen = event;
+  }
+
+  setFormFilterClass(): Object {
+    if (window.innerWidth > 1533.8) {
+      return {}
+    } else {
+      return {
+        "hidden": !this.isOpen,
+        "mt-2": this.isFixed
+      }
+    }
   }
 
   setFormService(event: FormService): void {
